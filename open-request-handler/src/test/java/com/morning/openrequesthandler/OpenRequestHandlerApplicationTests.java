178bbc3a788a7abb59e68d13910b3669517bc22b
morning <matveygromykhalin@gmail.com>
2024-09-11 14:03:37 +0300

    Все изменения описаны в obsidian

---
+++ open-request-handler/src/test/java/com/morning/openrequesthandler/OpenRequestHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morning.openrequesthandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenRequestHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

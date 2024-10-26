8a1ccd55c528afa298255996abec83bd7715ad64
morning <matveygromykhalin@gmail.com>
2024-10-26 20:58:07 +0300

    тест залива в develop

--- task-api-main/src/test/java/com/morning/taskapimain/unit/UserServiceTest.java
+++ task-api-main/src/test/java/com/morning/taskapimain/unit/UserServiceTest.java
@@ -113,7 +113,7 @@ class UserServiceTest {
 ///
         // Используем StepVerifier для проверки результата
         StepVerifier.create(result)
-                .expectNext("ADMI223N")
+                .expectNext("ADMI2233N")
                 .verifyComplete();
 
         // Проверяем, что username был извлечен из токена

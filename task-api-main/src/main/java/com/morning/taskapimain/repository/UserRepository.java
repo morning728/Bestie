58a2fefda62596f6ba951088ffab92f057b78228
morning <matveygromykhalin@gmail.com>
2024-10-26 19:43:55 +0300

    попытка настройки прохождения тестов при заливе

--- task-api-main/src/main/java/com/morning/taskapimain/repository/UserRepository.java
+++ task-api-main/src/main/java/com/morning/taskapimain/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.morning.taskapimain.repository;
 import com.morning.taskapimain.entity.User;
 import org.springframework.data.domain.Example;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
